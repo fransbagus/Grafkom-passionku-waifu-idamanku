fd666216a38e7ef2f0b081a81aad6a88d03c9799
SUI <52919996+SUUII@users.noreply.github.com>
2019-08-02 17:22:28 +0700

    SKRATADUUUUUU
    segitiga e masi kabur gan kalo mau dibantu silahkan saya mau makan dolo awoakwoakowoakoawkoawkowakkw

---
+++ rahasia super jangan dibuka selain pihak terpercaya/Source.cpp
@@ -0,0 +1,287 @@
+// _TugasRancang.cpp : Defines the entry point for the console application.
+//
+
+
+#include "windows.h"
+#include "stdlib.h"
+#include "glut.h"
+#include "Box2D\Box2D.h"
+#include <iostream>
+using namespace std;
+const float WIDTH = 800.0;
+const float HEIGHT = 600.0;
+
+const float m2p = 20; //meter to pixel
+const float p2m = 1 / m2p; //pixel to meter
+const float PI = 3.14;
+
+bool mouseDown;
+char keypress = 'a';
+
+float32 timeStep = 1 / 240.0;
+int32 velocityIteration = 8;
+int32 positionIteration = 3;
+
+//define physics world
+b2Vec2 gravity(0.0f, 9.81f);
+b2Vec2 gravity2(0.0f, 0.0f);
+bool cek = false;
+b2World* world; //pointer, dynamically allocated
+
+
+
+
+
+b2Body* addRectangle(int x, int y, int w, int h, bool dyn = true) { //add bodydef
+	//create dynamic body
+	b2BodyDef bodyDef;
+	if (dyn == true) {
+		bodyDef.type = b2_dynamicBody;
+	}
+	bodyDef.position.Set(x * p2m, y * p2m);
+	b2Body* body = world->CreateBody(&bodyDef);
+
+	//attach polygon using fixture def
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(w * p2m / 2, h * p2m / 2);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+	fixtureDef.density = 0.5f;
+	fixtureDef.friction = 0.1f;
+	fixtureDef.restitution = 0.15f;
+	body->CreateFixture(&fixtureDef);
+
+	return body;
+}
+
+b2Body* addCircle(int x, int y, int r, bool dyn = true) {
+	//create dynamic body
+	b2BodyDef bodyDef;
+	if (dyn == true) {
+		bodyDef.type = b2_dynamicBody;
+	}
+	bodyDef.position.Set(x * p2m, y * p2m);
+	b2Body* body = world->CreateBody(&bodyDef);
+
+	//attach circle using fixture def
+	b2CircleShape dynCircle;
+	dynCircle.m_radius = 10 * p2m;
+	dynCircle.m_p.Set(0, 0);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynCircle;
+	fixtureDef.density = 0.5f;
+	fixtureDef.friction = 0.1f;
+	fixtureDef.restitution = 0.15f;
+	body->CreateFixture(&fixtureDef);
+
+	return body;
+}
+
+
+b2Body* add3angle(int x, int y, int w, int h, bool dyn = true) { //add bodydef
+	//create dynamic body
+	b2BodyDef bodyDef;
+	if (dyn == true) {
+		bodyDef.type = b2_dynamicBody;
+	}
+	bodyDef.position.Set(x * p2m, y * p2m);
+	b2Body* body = world->CreateBody(&bodyDef);
+
+	//attach polygon using fixture def
+	b2Vec2 vertices[3];
+	vertices[0].Set(w * p2m /2, h * p2m /2);
+	vertices[1].Set(w * p2m *2, h * p2m /2);
+	vertices[2].Set(w * p2m, h * p2m *2);
+	int32 count = 3;
+
+	b2PolygonShape segitiga;
+	segitiga.Set(vertices, count);
+
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &segitiga;
+	fixtureDef.density = 0.5f;
+	fixtureDef.friction = 0.1f;
+	fixtureDef.restitution = 0.15f;
+	body->CreateFixture(&fixtureDef);
+
+	return body;
+}
+
+void drawSquare(b2Vec2* points, b2Vec2 center, float angle) {
+	glColor3f(1, 1, 1);
+	glPushMatrix();
+	glTranslatef(center.x * m2p, center.y * m2p, 0);
+	glRotatef(angle * 180.0 / PI, 0, 0, 1);
+	glBegin(GL_QUADS);
+	for (int i = 0; i < 4; i++) {
+		glVertex2f(points[i].x * m2p, points[i].y * m2p);
+	}
+	glEnd();
+	glPopMatrix();
+	glFlush();
+}
+
+void drawCircle(b2Vec2 center, float r, float angle) {
+	glColor3f(1, 1, 1);
+	glPushMatrix();
+	glTranslatef(center.x * m2p, center.y * m2p, 0);
+	glRotatef(angle * 180.0 / PI, 0, 0, 1);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(0, 0); //center point
+	for (float i = 0; i < 360; i++) {
+		glVertex2f((cos(i) * r) * m2p, (sin(i) * r) * m2p);
+	}
+	glEnd();
+	glPopMatrix();
+	glFlush();
+}
+
+void draw3angle(b2Vec2* points, b2Vec2 center, float angle) {
+	glColor3f(1, 1, 1);
+	glPushMatrix();
+	glTranslatef(center.x * m2p, center.y * m2p, 0);
+	glRotatef(angle * 180.0 / PI, 0, 0, 1);
+	glBegin(GL_POLYGON);
+	glVertex2f(points[0].x * m2p, points[0].y * m2p);
+	glVertex2f(points[1].x * m2p, points[1].y * m2p);
+	glVertex2f(points[2].x * m2p, points[2].y * m2p);
+
+
+	glEnd();
+	glPopMatrix();
+	glFlush();
+}
+
+void init()
+{
+	glMatrixMode(GL_PROJECTION);
+	//menyesuaikan sistem koordinat opengl
+	glOrtho(0, WIDTH, HEIGHT, 0, -1, 1);
+	//gluOrtho2D(0, WIDTH, HEIGHT, 0);
+	glViewport(0, WIDTH, 0, HEIGHT);
+	glMatrixMode(GL_MODELVIEW);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	world = new b2World(gravity2);
+
+
+	addRectangle(0, 600, 1600, 20, false);
+
+	glutPostRedisplay();
+}
+
+void handleKeypress(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'a':
+	case 'A':
+		glColor3f(1, 1, 1);
+		addRectangle(x, y, 20, 20, true);
+
+		world->Step(timeStep, velocityIteration, positionIteration);
+
+		glutSwapBuffers();
+		glutPostRedisplay();
+		break;
+	case's':
+	case'S':
+		glColor3f(1, 1, 1);
+		addCircle(x, y, 2000, true);
+
+		world->Step(timeStep, velocityIteration, positionIteration);
+
+		glutSwapBuffers();
+		glutPostRedisplay();
+		break;
+	case'd':
+	case 'D':
+		glColor3f(1, 1, 1);
+		add3angle(x, y, 20, 20, true);
+
+
+		world->Step(timeStep, velocityIteration, positionIteration);
+
+		glutSwapBuffers();
+		glutPostRedisplay();
+		break;
+	case'z':
+	case'Z':
+		world->SetGravity(gravity);
+		break;
+	}
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		mouseDown = true;
+	}else{
+		mouseDown = false;
+	}
+
+
+	printf("%d %d %d \n", state, x, y);
+}
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+	//untuk gambar semua bentuk
+	b2Body* tmp = world->GetBodyList();
+	b2Vec2 points[4];
+	while (tmp) {
+		cout << ((b2PolygonShape*)tmp->GetFixtureList()->GetShape())->GetVertexCount() << '\n';
+		if (tmp->GetFixtureList()->GetShape()->GetType() == 0) {
+			b2CircleShape* c = ((b2CircleShape*)tmp->GetFixtureList()->GetShape());
+			drawCircle(tmp->GetWorldCenter(), c->m_radius, tmp->GetAngle());
+		}
+		else{
+			for (int i = 0; i < ((b2PolygonShape*)tmp->GetFixtureList()->GetShape())->GetVertexCount(); i++)
+			{
+				points[i] = ((b2PolygonShape*)tmp->GetFixtureList()->GetShape())->GetVertex(i);
+			}
+			if (((b2PolygonShape*)tmp->GetFixtureList()->GetShape())->GetVertexCount()==4) {
+				drawSquare(points, tmp->GetWorldCenter(), tmp->GetAngle());
+			}
+			else {
+				draw3angle(points, tmp->GetWorldCenter(), tmp->GetAngle());
+			}
+		}
+		tmp = tmp->GetNext();
+	}
+
+
+	world->Step(timeStep, velocityIteration, positionIteration);
+
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
+
+
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Tugas Rancang");
+	init();
+	glutDisplayFunc(display);
+	glutMouseFunc(mouse);
+	glutKeyboardFunc(handleKeypress);
+
+	world->Step(timeStep, velocityIteration, positionIteration); //update frame
+	glutSwapBuffers();
+
+
+	/*
+	glutMotionFunc(mouseMotion);
+	glutReshapeFunc(handleResize);
+	glutTimerFunc(25, update, 0);*/
+
+	glutMainLoop();
+	return 0;
+}
+
---
+++ rahasia super jangan dibuka selain pihak terpercaya/imageloader.cpp
@@ -0,0 +1,169 @@
+#include <assert.h>
+#include <fstream>
+
+#include "imageloader.h"
+
+using namespace std;
+
+Image::Image(char* ps, int w, int h) : pixels(ps), width(w), height(h) {
+	
+}
+
+Image::~Image() {
+	delete[] pixels;
+}
+
+namespace {
+	//Konversi 4 buah karakter ke integer, menggunakan bentuk little-endian
+	int toInt(const char* bytes) {
+		return (int)(((unsigned char)bytes[3] << 24) |
+					 ((unsigned char)bytes[2] << 16) |
+					 ((unsigned char)bytes[1] << 8) |
+					 (unsigned char)bytes[0]);
+	}
+	
+	//Konversi 2 buah karakter ke integer, menggunakan bentuk little-endian
+	short toShort(const char* bytes) {
+		return (short)(((unsigned char)bytes[1] << 8) |
+					   (unsigned char)bytes[0]);
+	}
+	
+	//Membaca 4 byte selanjutnya sebagai integer, menggunakan bentuk little-endian
+	int readInt(ifstream &input) {
+		char buffer[4];
+		input.read(buffer, 4);
+		return toInt(buffer);
+	}
+	
+	short readShort(ifstream &input) {
+		char buffer[2];
+		input.read(buffer, 2);
+		return toShort(buffer);
+	}
+	
+	template<class T>
+	class auto_array {
+		private:
+			T* array;
+			mutable bool isReleased;
+		public:
+			explicit auto_array(T* array_ = NULL) :
+				array(array_), isReleased(false) {
+			}
+			
+			auto_array(const auto_array<T> &aarray) {
+				array = aarray.array;
+				isReleased = aarray.isReleased;
+				aarray.isReleased = true;
+			}
+			
+			~auto_array() {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+			}
+			
+			T* get() const {
+				return array;
+			}
+			
+			T &operator*() const {
+				return *array;
+			}
+			
+			void operator=(const auto_array<T> &aarray) {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+				array = aarray.array;
+				isReleased = aarray.isReleased;
+				aarray.isReleased = true;
+			}
+			
+			T* operator->() const {
+				return array;
+			}
+			
+			T* release() {
+				isReleased = true;
+				return array;
+			}
+			
+			void reset(T* array_ = NULL) {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+				array = array_;
+			}
+			
+			T* operator+(int i) {
+				return array + i;
+			}
+			
+			T &operator[](int i) {
+				return array[i];
+			}
+	};
+}
+
+Image* loadBMP(const char* filename) {
+	ifstream input;
+	input.open(filename, ifstream::binary);
+	assert(!input.fail() || !"File tidak ditemukan!!!");
+	char buffer[2];
+	input.read(buffer, 2);
+	assert(buffer[0] == 'B' && buffer[1] == 'M' || !"Bukan file bitmap!!!");
+	input.ignore(8);
+	int dataOffset = readInt(input);
+	
+	int headerSize = readInt(input);
+	int width;
+	int height;
+	switch(headerSize) {
+		case 40:
+			width = readInt(input);
+			height = readInt(input);
+			input.ignore(2);
+			assert(readShort(input) == 24 || !"Gambar tidak 24 bits per pixel!");
+			assert(readShort(input) == 0 || !"Gambar dikompres!");
+			break;
+		case 12:
+			width = readShort(input);
+			height = readShort(input);
+			input.ignore(2);
+			assert(readShort(input) == 24 || !"Gambar tidak 24 bits per pixel!");
+			break;
+		case 64:
+			assert(!"Tidak dapat mengambil OS/2 V2 bitmaps");
+			break;
+		case 108:
+			assert(!"Tidak dapat mengambil Windows V4 bitmaps");
+			break;
+		case 124:
+			assert(!"Tidak dapat mengambil Windows V5 bitmaps");
+			break;
+		default:
+			assert(!"Format bitmap ini tidak diketahui!");
+	}
+	
+	//Membaca data
+	int bytesPerRow = ((width * 3 + 3) / 4) * 4 - (width * 3 % 4);
+	int size = bytesPerRow * height;
+	auto_array<char> pixels(new char[size]);
+	input.seekg(dataOffset, ios_base::beg);
+	input.read(pixels.get(), size);
+	
+	//Mengambil data yang mempunyai format benar
+	auto_array<char> pixels2(new char[width * height * 3]);
+	for(int y = 0; y < height; y++) {
+		for(int x = 0; x < width; x++) {
+			for(int c = 0; c < 3; c++) {
+				pixels2[3 * (width * y + x) + c] =
+					pixels[bytesPerRow * y + 3 * x + (2 - c)];
+			}
+		}
+	}
+	
+	input.close();
+	return new Image(pixels2.release(), width, height);
+}
