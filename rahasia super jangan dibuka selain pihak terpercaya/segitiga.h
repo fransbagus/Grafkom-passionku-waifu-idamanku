1c7cb47ff05c96165aadf1f39d6d4a84268cbbae
SUI <52919996+SUUII@users.noreply.github.com>
2019-08-04 12:02:12 +0700

    segitiga udah bener gan kalo ga salah

--- rahasia super jangan dibuka selain pihak terpercaya/segitiga.h
+++ rahasia super jangan dibuka selain pihak terpercaya/segitiga.h
@@ -4,7 +4,7 @@ private:
 	const float p2m = 1 / m2p; //pixel to meter
 	const float PI = 3.14;
 public:
-	b2Body* add3angle(int x, int y, int w, int h, bool dyn = true);
+	b2Body* add3angle(int x, int y, float w, float h, bool dyn = true);
 	void draw3angle(b2Vec2* points, b2Vec2 center, float angle);
 	b2World* world;
 	segitiga(b2World* world2) {
@@ -12,20 +12,22 @@ public:
 	};
 };
 
-b2Body* segitiga::add3angle(int x, int y, int w, int h, bool dyn) { //add bodydef
+b2Body* segitiga::add3angle(int x, int y, float w, float h, bool dyn) { //add bodydef
 	//create dynamic body
 	b2BodyDef bodyDef;
 	if (dyn == true) {
 		bodyDef.type = b2_dynamicBody;
 	}
 	bodyDef.position.Set(x * p2m, y * p2m);
+	bodyDef.allowSleep = true;
+	bodyDef.awake = true;
 	b2Body* body = world->CreateBody(&bodyDef);
 
 	//attach polygon using fixture def
 	b2Vec2 vertices[3];
-	vertices[0].Set(0.0f, 0.0f);
-	vertices[1].Set(1.0f, 0.0f);
-	vertices[2].Set(0.0f, 1.0f);
+	vertices[0].Set(-w/2 * p2m, h/2 * p2m);
+	vertices[1].Set(w/2 * p2m, h/2 * p2m);
+	vertices[2].Set(0, -h/2 * p2m);
 	int32 count = 3;
 
 	b2PolygonShape segitiga;
@@ -49,10 +51,9 @@ void segitiga::draw3angle(b2Vec2* points, b2Vec2 center, float angle) {
 	glRotatef(angle * 180.0 / PI, 0, 0, 1);
 	glBegin(GL_POLYGON);
 
-	glVertex2f(points[0].x * m2p - 6.75, points[0].y * m2p - 6.75);
-	glVertex2f(points[1].x * m2p - 6.75, points[1].y * m2p - 6.75);
-	glVertex2f(points[2].x * m2p - 6.75, points[2].y * m2p - 6.75);
-
+	glVertex2f(points[0].x * m2p, points[0].y * m2p);
+	glVertex2f(points[1].x * m2p, points[1].y * m2p);
+	glVertex2f(points[2].x * m2p, points[2].y * m2p);
 
 	glEnd();
 	glPopMatrix();
